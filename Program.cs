96b7159b55a538790fef02249f1579de140ea8db
diana5907 <127054819+diana5907@users.noreply.github.com>
2024-04-11 20:26:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            // Зчитування чисел з файлу
+            string[] numbers = File.ReadAllText(@"D:/Дз/Програмування/111.txt").Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Перетворення рядка чисел в масив цілих чисел та знаходження їх суми
+            int[] intNumbers = Array.ConvertAll(numbers, int.Parse);
+            int sum = intNumbers.Sum();
+
+            // Виведення суми елементів масиву
+            Console.WriteLine($"Сума елементів масиву: {sum}");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Файл не знайдено.");
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("Помилка читання з файлу.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Файл містить некоректні дані.");
+        }
+
+        Console.ReadLine(); // Чекаємо на натискання клавіші перед завершенням програми
+    }
+}
